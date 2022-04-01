f5b6f9670fcfae83dad466bb5e76f7c1d732c4ed
Mateus Florêncio <mateuusflorencio@outlook.com.br>
2022-04-01 20:01:33 -0300

    rotas

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import routerCheckList from "./src/routes/checklist.js"
 const app = express();
 app.use(express.json());
 
-app.use(routerCheckList);
+app.use("/checklists", routerCheckList);
 
 app.listen(3030, () => {
     console.log("Servidor iniciado")
--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -2,14 +2,28 @@ import express from "express";
 
 const routerCheckList = express.Router();
 
-routerCheckList.get("/checklists", (req, res) => {
+routerCheckList.get("/", (req, res) => {
     console.log("checklist funcionadno");
     res.send();
 });
 
-routerCheckList.post("/checklists", (req, res) => {
+routerCheckList.get("/:id", (req, res) => {
+    console.log(req.params.id);
+});
+
+routerCheckList.post("/", (req, res) => {
     console.log(req.body);
     res.status(201).send(req.body);
+});
+
+routerCheckList.put("/:id", (req, res) => {
+    console.log(req.params.id);
+    res.status(200).send(req.params.id)
+});
+
+routerCheckList.delete("/:id", (req, res) => {
+    console.log(req.params.id);
+    res.send().status(200);
 });
 
 export default routerCheckList;
